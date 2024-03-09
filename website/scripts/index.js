ef66a4ff7520ee527982f89dcc125540afcfebe8
Gabriela Paulini Deutner <deutnerg@gmail.com>
2024-03-09 16:02:08 -0300

    menu hamburguer responsivo

---
+++ website/scripts/index.js
@@ -0,0 +1,8 @@
+const navbar = document.querySelector('.navbar');
+const icon = document.querySelector('.menu-icon');
+
+
+icon.addEventListener('click', function(){
+    navbar.classList.toggle('show');
+}
+);
